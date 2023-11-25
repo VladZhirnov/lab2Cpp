d0bc8da9ab4fa4abf2a5c691a9d4bacdaa5f5726
Влад Жирнов <vlad-zhirnov-1995@mail.ru>
2023-11-25 16:30:05 +0400

    The LinkedList class and the Node structure have been created. Added constructors, destructor and copy operation.

--- include/functions/header.h
+++ include/functions/header.h
@@ -1,302 +1,107 @@
-﻿#include <random>
-#include <iostream>
-#include <cmath>
-#include <complex>
-#include <math.h>
-#define _USE_MATH_DEFINES
-# define M_PI 3.14159265358979323846
-
+﻿#include <iostream>
+#include <random>
 
 template <typename T>
-T Random(T a, T b) {
-	std::random_device random_device;
-	std::mt19937 generator(random_device());
-	std::uniform_real_distribution<double> distribution(a, b);
-	T res = static_cast<T>(distribution(generator));
-	return res;
-}
+struct Node {
+    T data;
+    Node* next;
 
-template<typename T>
-std::complex<T> Random(std::complex<T> a, std::complex<T> b) {
-	T real1 = std::min(a.real(), b.real());
-	T real2 = std::max(a.real(), b.real());
-	T imag1 = std::min(a.imag(), b.imag());
-	T imag2 = std::max(a.imag(), b.imag());
-	std::random_device random_device;
-	std::mt19937 generator(random_device());
-	std::uniform_real_distribution<T> distribution_real(real1, real2);
-	T x = distribution_real(generator);
-	std::uniform_real_distribution<T> distribution_imag(imag1, imag2);
-	T y = distribution_imag(generator);
-	std::complex<T> point(x, y);
-	return point;
-}
+    Node(const T& value) : data(value), next(nullptr) {}
+};
 
 template <typename T>
-struct Point {
+class LinkedList {
 private:
-	T _x, _y;
+    Node<T>* head;
+
 public:
-	Point();
-	Point(T x, T y);
-	T get_x() const;
-	T get_y() const;
-	double distance(const Point<T>* point);
-	double distance_complex(const Point<T>* point);
+    LinkedList();
+    LinkedList(const LinkedList<T>& list);
+    LinkedList(int size);
+    ~LinkedList();
+    LinkedList<T>& operator=(const LinkedList<T>& list);
+    void swap(LinkedList<T>& list);
+    template<typename U>
+    friend std::ostream& operator<<(std::ostream& stream, const LinkedList<U>& list);
 };
 
 template <typename T>
-class broken_line {
-	Point<T>** _points;
-	int _size;
-public:
-	broken_line(const broken_line<T>& other);
-	broken_line();
-	broken_line(int size);
-	broken_line(T low, T hight, int count);
-	~broken_line();
-	Point<T> operator[](int index) const;
-	Point<T>& operator[](int index);
-	broken_line<T>& operator=(const broken_line<T>& rhs);
-	broken_line<T>& operator+=(const broken_line<T>& other);
-	broken_line<T> operator+(const broken_line<T>& other);
-	broken_line<T>& operator+=(const Point<T>& point);
-	broken_line<T> operator+(const Point<T>& point);
-	int size() const;
-	void swap(broken_line<T>& other);
-	void push_point(const Point<T>& point);
-	double length();
-	double length_complex();
-	void make_polygon(const int N, const double radius);
-};
+LinkedList<T>::LinkedList() : head(nullptr) {}
 
 template <typename T>
-broken_line<T>::broken_line(const broken_line<T>& other) {
-	_points = new Point<T>*[other._size];
-	_size = other._size;
-	for (int i = 0; i < _size; i++)
-	{
-		_points[i] = new Point<T>(other[i]);
-	}
+LinkedList<T>::LinkedList(const LinkedList<T>& list) {
+    Node<T>* tmp1 = list.head;
+    Node<T>* tmp2 = nullptr;
+    head = nullptr;
+    while (tmp1) {
+        Node<T>* newNode = new Node<T>(tmp1->data);
+        if (!head) {
+            head = newNode;
+            tmp2 = head;
+        }
+        else {
+            tmp2->next = newNode;
+            tmp2 = tmp2->next;
+        }
+        tmp1 = tmp1->next;
+    }
 }
 
 template <typename T>
-broken_line<T>::broken_line() {
-	_points = nullptr;
-	_size = 0;
+LinkedList<T>::LinkedList(int size) {
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<T> dis(0, 9);
+
+    head = nullptr;
+
+    for (int i = 0; i < size; ++i) {
+        T randomValue = dis(gen);
+        Node<T>* newNode = new Node<T>(randomValue);
+
+        if (!head) {
+            head = newNode;
+        }
+        else {
+            newNode->next = head;
+            head = newNode;
+        }
+    }
 }
 
 template <typename T>
-broken_line<T>::broken_line(int size) {
-	_size = size;
+LinkedList<T>::~LinkedList() {
+    Node<T>* tmp = head;
+    Node<T>* next;
+    while (tmp) {
+        next = tmp->next;
+        delete tmp;
+        tmp = next;
+    }
+    head = nullptr;
 }
 template <typename T>
-broken_line<T>::broken_line(T low, T hight, int count) {
-	_points = new Point<T>*[count];
-	for (int i = 0; i < count; i++)
-	{
-		T x = Random(low, hight);
-		T y = Random(low, hight);
-		Point<T> a(x, y);
-		_points[i] = new Point<T>(a);
-	}
-	_size = count;
-}
-
-template <typename T>
-Point<T>::Point() {
-	_x = 0;
-	_y = 0;
+LinkedList<T>& LinkedList<T>::operator=(const LinkedList<T>& list) {
+    if (!*this == list) {
+        LinkedList<T> copy(list);
+        copy.swap(*this);
+    }
+    return *this;
 }
 
 template <typename T>
-Point<T>::Point(T x, T y) {
-	_x = x;
-	_y = y;
+void LinkedList<T>::swap(LinkedList<T>& list) {
+    std::swap(head, list.head);
 }
 
 template <typename T>
-T Point<T>::get_x() const {
-	return _x;
-}
-
-template <typename T>
-T Point<T>::get_y() const {
-	return _y;
-}
-
-template <typename T>
-Point<T> broken_line<T>::operator[](int index) const {
-	if (index < 0 || index > _size) {
-		throw std::out_of_range("Invalid index");
-	}
-	return *_points[index];
-}
-
-template <typename T>
-Point<T>& broken_line<T>::operator[](int index)
-{
-	if (index < 0) {
-		throw std::out_of_range("Invalid index");
-	}
-	return *_points[index];
-}
-
-template <typename T>
-broken_line<T>::~broken_line()
-{
-	for (int i = 0; i < _size; ++i) {
-		delete _points[i];
-	}
-	delete[] _points;
-}
-
-template <typename T>
-broken_line<T>& broken_line<T>::operator=(const broken_line<T>& rhs) {
-	if (*this != rhs) {
-		broken_line<T> copy(rhs);
-		copy.swap(*this);
-	}
-	return *this;
-
-}
-
-template <typename T>
-void broken_line<T>::swap(broken_line<T>& other) {
-	std::swap(_points, other._points);
-	std::swap(_size, other._size);
-}
-
-template<typename T>
-int broken_line<T>::size() const {
-	return _size;
-}
-
-template<typename T>
-std::ostream& operator<<(std::ostream& stream, const Point<T>& point) {
-	stream << "(" << point.get_x() << ", " << point.get_y() << ")";
-	return stream;
-}
-
-template<typename T>
-std::ostream& operator<<(std::ostream& stream, const broken_line<T>& line) {
-	stream << "[ ";
-	for (int i = 0; i < line.size(); ++i) {
-		if (i == (line.size() - 1)) {
-			stream << line[i];
-		}
-		else {
-			stream << line[i] << ", ";
-		}
-	}
-	stream << " ]";
-	return stream;
-}
-
-template<typename T>
-broken_line<T>& broken_line<T>::operator+=(const broken_line<T>& other) {
-	int capacity = _size + other._size;
-	Point<T>** temp = new Point<T>*[capacity]();
-	for (int i = 0; i < _size; ++i) {
-		temp[i] = _points[i];
-	}
-	delete[] _points;
-	_points = temp;
-	for (int i = 0; i < other._size; ++i) {
-		*this += *other._points[i];
-	}
-	return *this;
-}
-
-template<typename T>
-broken_line<T> broken_line<T>::operator+(const broken_line<T>& other) {
-	broken_line<T> add_res(*this);
-	add_res += other;
-	return add_res;
-}
-
-template <typename T>
-broken_line<T>& broken_line<T>::operator+=(const Point<T>& point) {
-	auto new_points = new Point<T>*[_size + 1];
-	for (int i = 0; i < _size; i++)
-	{
-		new_points[i] = _points[i];
-	}
-	new_points[_size] = new Point<T>(point);
-	delete[] _points;
-	_points = new_points;
-	_size++;
-	return *this;
-}
-
-template<typename T>
-broken_line<T> broken_line<T>::operator+(const Point<T>& point) {
-	broken_line<T> add_res(*this);
-	add_res += point;
-	return add_res;
-}
-
-//Äîáàâëåíèå òî÷êè â íà÷àëî
-template <typename T>
-void broken_line<T>::push_point(const Point<T>& point) {
-	auto new_points = new Point<T>*[_size + 1];
-	for (int i = _size; i > 0; i--)
-	{
-		new_points[i] = _points[i - 1];
-	}
-	new_points[0] = new Point<T>(point);
-	_size++;
-	delete[] _points;
-	_points = new_points;
-}
-
-template <typename T>
-double Point<T>::distance(const Point<T>* point) {
-	double dx = point->get_x() - _x;
-	double dy = point->get_y() - _y;
-	return std::sqrt(dx * dx + dy * dy);
-}
-
-template <typename T>
-double broken_line<T>::length() {
-	double length_line = 0;
-	for (int i = 1; i < _size; i++) {
-		length_line += _points[i - 1]->distance(_points[i]);
-	}
-	return length_line;
-}
-
-template <typename T>
-double Point<T>::distance_complex(const Point<T>* point) {
-	T delta = point->get_x() - _x;
-	T delta1 = point->get_y() - _y;
-	return std::abs(delta * delta + delta1 * delta1);
-}
-
-template <typename T>
-double broken_line<T>::length_complex() {
-	double length_line = 0;
-	for (int i = 1; i < _size; i++) {
-		length_line += _points[i - 1]->distance_complex(_points[i]);
-	}
-	return length_line;
-}
-
-//Çàäà÷à ¹1
-template <typename T>
-void broken_line<T>::make_polygon(const int N, const double radius) {
-	// N - Çàäàííîå êîëè÷åñòâî âåðøèí N äëÿ N-óãîëüíèêà
-	// radius - Ðàäèóñ îêðóæíîñòè, âïèñàííîé â N-óãîëüíèê
-	if (N < 2 || N == 2) {
-		throw std::invalid_argument("N >= 2!");
-	}
-	broken_line<T> polygon;
-	double angle = 2 * M_PI / N;  // Óãîë ìåæäó âåðøèíàìè N-óãîëüíèêà
-	for (int i = 0; i < N; i++) {
-		T x = radius * std::cos(i * angle);
-		T y = radius * std::sin(i * angle);
-		Point<T> point(x, y);
-		polygon += point;
-	}
-	std::cout << polygon << std::endl;
+std::ostream& operator<<(std::ostream& stream, const LinkedList<T>& list) {
+    stream << "[";
+    Node<T>* tmp = list.head;
+    while (tmp) {
+        stream << "(" << tmp->data << ")";
+        tmp = tmp->next;
+    }
+    stream << "]";
+    return stream;
 }
--- src/main.cc
+++ src/main.cc
@@ -3,38 +3,8 @@
 #include <complex>
 
 int main() {
-    Point<int> t(2, 1);
-    Point<double> t1(2, 1);
-    std::cout << t << std::endl;
-    double f1 = 4;
-    double f2 = 7;
-    double f3 = 1;
-    double f4 = 5;
-    broken_line<double> a(f1, f2, 4);
-    broken_line<double> b(f3, f4, 5);
-    std::cout << a << std::endl;
-    std::cout << b << std::endl;
-    broken_line<double> c = a + b;
-    c.push_point(t1);
-    std::cout << c << std::endl;
-    auto len_line = c.length();
-    std::cout << len_line << std::endl;
-    broken_line<double> pol;
-    pol.make_polygon(6, 5);
-    //Ñ êîìïëåêñíûìè ÷èñëàìè :
-    //std::complex <double> c(4, 2);
-    //std::complex <double> v(1, 8);
-    //std::complex <double> y(3, 1);
-    //std::complex <double> u(5, 6);
-    //Point<std::complex <double>> k(y, u);
-    //Point<std::complex <double>> j(c, v);
-    //broken_line< std::complex <double>> d(c, v, 5);
-    //broken_line< std::complex <double>> q(y, u, 7);
-    //std::cout << d << std::endl;
-    //broken_line< std::complex <double>> m = d + q;
-    //std::cout << m << std::endl;
-    //auto len_line = d.length_complex();
-    //std::cout << len_line << std::endl;
-    //broken_line<std::complex <double>> pol;
-    //pol.make_polygon(6, 5);
+    LinkedList<int> randomList1(5);
+    std::cout << randomList1 << std::endl;
+    LinkedList<int> randomList2 = randomList1;
+    std::cout << randomList2 << std::endl;
 }
